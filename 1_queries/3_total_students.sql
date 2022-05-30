0c539f93f4e0f25f8eed583f4770d74bab81cb12
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-30 12:50:24 -0400

    BootcampX Queries 1

---
+++ 1_queries/3_total_students.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*)
+FROM students
+WHERE cohort_id > 0 AND cohort_id <= 3;
---
+++ 1_queries/5_currently_enrolled.sql
@@ -0,0 +1,4 @@
+SELECT name, id, cohort_id, end_date
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
