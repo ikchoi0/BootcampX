c8ea02ea9386da5aae854736d813a83bb8e8de35
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-01 11:01:18 -0400

    SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,14 +10,13 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log(args);
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1]}
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`, [`${args[0]}%`, args[1]])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
